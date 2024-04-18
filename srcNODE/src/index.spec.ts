a8e2c1147e5ac5a8ea47b55c7410a7c45703053c
(Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
2024-04-19 06:17:31 +0800

    srcNODE: updated to match srcANGULAR standards
    Since we want maximum compatibilty with Angular (srcANGULAR) tech,
    we have to update the current srcNODE to match its standards. Hence,
    let's do this.
    
    This patch updates srcNODE/ to match srcANGULAR/ standards in srcNODE/
    directory.
    
    Co-authored-by: Shuralyov, Jean <jean.shuralyov@proton.me>
    Co-authored-by: Galyna, Cory <cory.galyna@gmail.com>
    Co-authored-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>
    Signed-off-by: (Holloway) Chew, Kean Ho <hollowaykeanho@gmail.com>

--- srcNODE/src/index.spec.ts
+++ srcNODE/src/index.spec.ts
@@ -1,9 +1,7 @@
 import { hello } from "./index.js";
 
-import * as assert from 'assert';
-
 describe('hello', () => {
 	it('should perform a proper printout', () => {
-		assert.equal(hello("World"), "Hello World");
+		expect(hello("World")).toBe("Hello World");
 	});
 });
